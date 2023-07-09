c7ccf3e93682044fd642f76b786f0016e98c481f
Nick Tesh <56547918+nicktesh@users.noreply.github.com>
2023-07-09 01:31:39 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -33,24 +33,62 @@ if (document.querySelector(".mobile-menu")) {
   });
 }
 // Home Page Banner
-const banner = document.getElementById("banner");
 const toggleButton = document.getElementById("toggleButton");
-const menuToggleButton = document.querySelector(".mobile-menu-toggle");
-const mainLogo = document.querySelector(".main-logo");
-const heroBGDesktop = document.querySelector(".hero-background.desktop");
-const heroBGMobile = document.querySelector(".hero-background.mobile");
+const body = document.body;
+const currentMode = localStorage.getItem("mode");
 
+// Checks local storage for time of day
+if (currentMode === "nighttime") {
+  body.classList.add("nighttime");
+  toggleButton.innerText = "Daytime";
+} else {
+  localStorage.setItem("mode", "daytime");
+  body.classList.remove("nighttime");
+  toggleButton.innerText = "Nighttime";
+}
+
+// On click of time of day button, switch between day or night
 toggleButton.addEventListener("click", () => {
-  banner.classList.toggle("nighttime");
-  menuToggleButton.classList.toggle("dark");
-  mainLogo.classList.toggle("dark");
+  body.classList.toggle("nighttime");
+
+  if (currentMode === "nighttime") {
+    localStorage.setItem("mode", "daytime");
+  } else {
+    localStorage.setItem("mode", "nighttime");
+  }
+
   if (toggleButton.innerText === "Nighttime") {
     toggleButton.innerText = "Daytime";
-    heroBGDesktop.src = "images/mountains-night.svg";
-    heroBGMobile.src = "images/mountains-night-mobile.svg";
   } else {
     toggleButton.innerText = "Nighttime";
-    heroBGDesktop.src = "images/mountains-day.svg";
-    heroBGMobile.src = "images/mountains-day-mobile.svg";
   }
 });
+
+// Random Value Function for Animations
+function applyRandomValueToElements(selector, className) {
+  const elements = document.querySelectorAll(selector);
+  elements.forEach((element) => {
+    element.style.setProperty("--random-value", Math.random());
+  });
+}
+
+// Assets in main hero background
+applyRandomValueToElements("#trees-left .cls-4");
+applyRandomValueToElements("#trees-right .cls-4");
+applyRandomValueToElements("#trees-3 .cls-7");
+applyRandomValueToElements("#stars .cls-11");
+
+// Update hero background view box
+const mediaQuery = window.matchMedia("(max-width: 768px)");
+const svgElement = document.getElementById("mountains-bg");
+
+function updateViewBox() {
+  if (mediaQuery.matches) {
+    svgElement.setAttribute("viewBox", "700 0 992 850");
+  } else {
+    svgElement.setAttribute("viewBox", "0 0 1920 850");
+  }
+}
+
+mediaQuery.addEventListener("change", updateViewBox);
+updateViewBox();
